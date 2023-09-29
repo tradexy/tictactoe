1b253391fa9de3754a3124624fa1ec57dcfa1d5a
tradexy <tradexy@gmail.com>
2023-09-29 13:37:30 +0100

    style and sound

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ const board = Array.from(Array(9).keys());
 document.querySelectorAll('.box').forEach(box => box.addEventListener('click', boxClicked));
 document.getElementById('reset').addEventListener('click', resetGame);
 
+// Auto-start the game
+resetGame();
+
 function boxClicked(e) {
   const id = e.target.id;
   if (!board[id]) {
     board[id] = currentPlayer;
     document.getElementById(id).innerText = currentPlayer;
-    if (checkWin()) {
+    const winningCombination = checkWin();
+    if (winningCombination) {
       alert(`${currentPlayer} wins!`);
+      // Play the winning sound here (uncomment the next line)
+      // winningSound.play();
+      winningCombination.forEach(index => {
+        document.getElementById(index).style.backgroundColor = 'red';
+      });
       resetGame();
     } else if (board.every(cell => cell)) {
       alert('Draw!');
@@ -31,10 +40,18 @@ function checkWin() {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  return winCombination.some(combination => combination.every(index => board[index] === currentPlayer));
+  for (let combination of winCombination) {
+    if (combination.every(index => board[index] === currentPlayer)) {
+      return combination;
+    }
+  }
+  return null;
 }
 
 function resetGame() {
   board.fill(null);
-  document.querySelectorAll('.box').forEach(box => box.innerText = '');
+  document.querySelectorAll('.box').forEach(box => {
+    box.innerText = '';
+    box.style.backgroundColor = '';
+  });
 }
